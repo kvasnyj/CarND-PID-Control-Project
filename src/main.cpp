20faf1e455155a4adb18edec070664dc034532fe
Roman <kvasnyj@gmail.com>
2017-05-08 21:42:30 +0400

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -30,79 +30,118 @@ std::string hasData(std::string s) {
     return "";
 }
 
-double run(PID &pid_, int n = 100) {
-    PID pid = pid_;
-    uWS::Hub h;
-    int i = 0;
+void move(uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode, bool print, PID &pid)
+{
+    auto s = hasData(std::string(data).substr(0, length));
+    if (s != "") {
+        auto j = json::parse(s);
+        std::string event = j[0].get<std::string>();
+        if (event == "telemetry") {
+            // j[1] is the data JSON object
+            double cte = std::stod(j[1]["cte"].get<std::string>());
+            double speed = std::stod(j[1]["speed"].get<std::string>());
+            double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+            double steer_value;
 
-    h.onMessage([&pid, &i, &n, &h](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-        // "42" at the start of the message means there's a websocket message event.
-        // The 4 signifies a websocket message
-        // The 2 signifies a websocket event
-        if (length && length > 2 && data[0] == '4' && data[1] == '2') {
-            if (i == 0) {
-                std::string reset_msg = "42[\"reset\", {}]";
-                ws.send(reset_msg.data(), reset_msg.length(), uWS::OpCode::TEXT);
-            }
-            std::cout << i << std::endl;
-            if (++i > n && n > 0) {
-                // https://github.com/uWebSockets/uWebSockets/issues/366
-                auto uv_async_callback = [](uv_async_t* handle) {
-                    auto uv_walk_callback = [](uv_handle_t* handle, void* /*arg*/) {
-                        if (!uv_is_closing(handle))
-                            uv_close(handle, nullptr);
-                    };
+            steer_value = pid.UpdateError(cte);
 
-                    auto loop = handle->loop;
-                    auto hub = (uWS::Hub *)handle->data;
-                    //hub->getDefaultGroup<true>().close();
+            // DEBUG
+            if (print)
+                std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
-                    uv_stop(loop);
-                    uv_walk(loop, uv_walk_callback, nullptr);
-                    uv_run(loop, UV_RUN_DEFAULT);
-                    uv_loop_close(loop);
-                };
-
-                auto loop = h.getLoop();
-
-                uv_async_t asyncHandle;
-                asyncHandle.data = &h;
-                uv_async_init(loop, &asyncHandle, uv_async_callback);
-                uv_async_send(&asyncHandle);
-
-                return;
-            }
-
-            auto s = hasData(std::string(data).substr(0, length));
-            if (s != "") {
-                auto j = json::parse(s);
-                std::string event = j[0].get<std::string>();
-                if (event == "telemetry") {
-                    // j[1] is the data JSON object
-                    double cte = std::stod(j[1]["cte"].get<std::string>());
-                    double speed = std::stod(j[1]["speed"].get<std::string>());
-                    double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-                    double steer_value;
-
-                    steer_value = pid.UpdateError(cte);
-
-                    // DEBUG
-                    if (n == 0) std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
-
-                    json msgJson;
-                    msgJson["steering_angle"] = steer_value;
-                    msgJson["throttle"] = 0.3;
-                    auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-                    if (n == 0) std::cout << msg << std::endl;
-                    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-                }
-            } else {
-                // Manual driving
-                std::string msg = "42[\"manual\",{}]";
-                ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-            }
+            json msgJson;
+            msgJson["steering_angle"] = steer_value;
+            msgJson["throttle"] = 0.3;
+            auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+            if (print) std::cout << msg << std::endl;
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
-    });
+    } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+    }
+}
+
+void run(double p[], bool optimize) {
+    PID pid;
+    pid.Init(p[0], p[1], p[2]);
+
+    uWS::Hub h;
+    int i = 0, it=0, par=0;
+    double dp[] = {.1, .01, 1.};
+    int state = 0;
+    double best_err = 0;
+
+    h.onMessage(
+            [&pid, &i, &dp, &state, &best_err, &it, &par, &optimize, &p](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+                // "42" at the start of the message means there's a websocket message event.
+                // The 4 signifies a websocket message
+                // The 2 signifies a websocket event
+                if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+                    if (i >= 100 && optimize) {
+                        i = 0;
+                        it++;
+
+                        double err = pid.TotalError();
+                        double sum_dp = std::accumulate(std::begin(dp), std::end(dp), 0.0, std::plus<double>());
+
+                        std::cout << "iteration: " << it << ", error: " << err << ", sum_dp: " << sum_dp << std::endl;
+                        std::cout << "Kp = " << p[0] << ", Ki = " << p[1] << ", Kd = " << p[2] << std::endl;
+
+                        //twiddle
+                        if (sum_dp > 0.2) {
+                            //std::cout << "state = " << state << ", par = " << par << std::endl;
+                            switch (state) {
+                                case 0: // init
+                                    best_err = err;
+                                    state = 1;
+                                    p[par] += dp[par];
+                                    pid.Init(p[0], p[1], p[2]);
+                                    break;
+                                case 1:
+                                    if (err < best_err) {
+                                        best_err = err;
+                                        dp[par] *= 1.1;
+
+                                        par = (par+1)%3;
+                                        p[par] += dp[par];
+                                        pid.Init(p[0], p[1], p[2]);
+                                    } else {
+                                        p[par] -= 2 * dp[par];
+                                        pid.Init(p[0], p[1], p[2]);
+                                        state = 2;
+                                    }
+                                    break;
+                                case 2:
+                                    if (err < best_err) {
+                                        best_err = err;
+                                        dp[par] *= 1.1;
+                                    } else {
+                                        p[par] += dp[par];
+                                        dp[par] *= 0.9;
+                                    }
+                                    par = (par+1)%3;
+                                    state = 1;
+                                    p[par] += dp[par];
+                                    pid.Init(p[0], p[1], p[2]);
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            optimize = false;
+                        }
+                    }
+
+                    if (i++ == 0) {
+                        std::string reset_msg = "42[\"reset\", {}]";
+                        ws.send(reset_msg.data(), reset_msg.length(), uWS::OpCode::TEXT);
+                    }
+
+                    move(ws, data, length, opCode, !optimize, pid);
+                }
+            });
 
     // We don't need this since we're not using HTTP but if it's removed the program
     // doesn't compile :-(
@@ -133,66 +172,13 @@ double run(PID &pid_, int n = 100) {
     }
 
     h.run();
-
-    //h.getDefaultGroup<true>().close();
-
-    return pid.TotalError();
 }
 
-void twiddle() {
-    //constant
-    int n = 10;
-    double tol = 0.2;
-
-    //variables
-    int it = 0;
-    double sum_dp = 3;
-    int p[] = {0, 0, 0};
-    int dp[] = {1, 1, 1};
-    double err = 0;
-
-    //init PID
-    PID pid;
-    pid.Init(p[0], p[1], p[2]);
-    double best_err = run(pid, n);
-
-    while (sum_dp > tol) {
-
-        for (int i = 0; i < 3; i++) {
-            p[i] += dp[i];
-            pid.Init(p[0], p[1], p[2]);
-            err = run(pid, n);
-
-            if (err < best_err) {
-                best_err = err;
-                dp[i] *= 1.1;
-            } else {
-                p[i] -= 2 * dp[i];
-                pid.Init(p[0], p[1], p[2]);
-                err = run(pid, n);
-
-                if (err < best_err) {
-                    best_err = err;
-                    dp[i] *= 1.1;
-                } else {
-                    p[i] += dp[i];
-                    dp[i] *= 0.9;
-                }
-            }
-        }
-
-
-        sum_dp = std::accumulate(std::begin(dp), std::end(dp), 0, std::plus<double>());
-        std::cout << "iteration: " << it++ << " error: " << err << "sum_dp: " << sum_dp << std::endl;
-        std::cout << "Kp = " + p[0] << ", Ki = " << p[1] << ", Kd = " << p[2] << std::endl;
-    }
-
-    std::cout << "Kp = " + p[0] << ", Ki = " << p[1] << ", Kd = " << p[2] << std::endl;
-}
 
 int main() {
-    twiddle();
+    double p[] = {0, 0, 0};
+    //double p[] = {0.2, 0.004, 3.0};
+    run(p, true);
 
     return 0;
-
 }
